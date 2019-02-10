fcd39e05e28fb13f3344e23f4f568bbe25278d42
eskerda <eskerda@gmail.com>
2019-02-10 02:22:57 +0100

    Add redis helper on specs

--- spec/corax/01-access_spec.lua
+++ spec/corax/01-access_spec.lua
@@ -1,13 +1,10 @@
 local helpers = require "spec.helpers"
 local version = require("version").version
 
+local redis = require "spec.redis"
+
 local PLUGIN_NAME = require("kong.plugins.corax").PLUGIN_NAME
 local KONG_VERSION = version(select(3, assert(helpers.kong_exec("version"))))
-
-local REDIS_HOST     = "127.0.0.1"
-local REDIS_PORT     = 6379
-local REDIS_PASSWORD = ""
-local REDIS_DATABASE = 0
 
 local DEFAULT_ROUTE_HOST             = "test1.com"
 local VARY_QUERY_PARAMS_ROUTE_HOST   = "test2.com"
@@ -33,41 +30,6 @@ end
 local function test_is_bypass(res)
   cache_is_status(res, "Bypass")
 end
-
-local function redis_connection()
-  local redis = require "resty.redis"
-  local red = redis:new()
-  red:set_timeout(2000)
-  local ok, err = red:connect(REDIS_HOST, REDIS_PORT)
-  if not ok then
-    error("failed to connect to Redis: " .. err)
-  end
-
-  if REDIS_PASSWORD and REDIS_PASSWORD ~= "" then
-    local ok, err = red:auth(REDIS_PASSWORD)
-    if not ok then
-      error("failed to connect to Redis: " .. err)
-    end
-  end
-
-  local ok, err = red:select(REDIS_DATABASE)
-  if not ok then
-    error("failed to change Redis database: " .. err)
-  end
-  return red
-end
-
-local redis = {
-  connection = redis_connection,
-  flush = function()
-    local red = redis_connection()
-    red:flushall()
-    red:close()
-  end,
-  keys = function(red)
-    return red:keys(PLUGIN_NAME .. "-*")
-  end
-}
 
 local function request(method, url, opts, res_status)
   local client = helpers.proxy_client()
---
+++ spec/redis.lua
@@ -0,0 +1,50 @@
+local helpers = require "spec.helpers"
+
+local PLUGIN_NAME = require("kong.plugins.corax").PLUGIN_NAME
+
+local REDIS_HOST     = "127.0.0.1"
+local REDIS_PORT     = 6379
+local REDIS_PASSWORD = ""
+local REDIS_DATABASE = 0
+
+local function redis_connection()
+  local redis = require "resty.redis"
+  local red = redis:new()
+  red:set_timeout(2000)
+  local ok, err = red:connect(REDIS_HOST, REDIS_PORT)
+  if not ok then
+    error("failed to connect to Redis: " .. err)
+  end
+
+  if REDIS_PASSWORD and REDIS_PASSWORD ~= "" then
+    local ok, err = red:auth(REDIS_PASSWORD)
+    if not ok then
+      error("failed to connect to Redis: " .. err)
+    end
+  end
+
+  local ok, err = red:select(REDIS_DATABASE)
+  if not ok then
+    error("failed to change Redis database: " .. err)
+  end
+  return red
+end
+
+local redis = {
+  REDIS_HOST = REDIS_HOST,
+  REDIS_PORT = REDIS_PORT,
+  REDIS_PASSWORD = REDIS_PASSWORD,
+  REDIS_DATABASE = REDIS_DATABASE,
+  connection = redis_connection,
+  flush = function()
+    local red = redis_connection()
+    red:flushall()
+    red:close()
+  end,
+  keys = function(red, query)
+    query = query or PLUGIN_NAME .. "-*"
+    return red:keys(query)
+  end,
+}
+
+return redis
